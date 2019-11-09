b8a7eb2471891515b13a6b2114d31e3eb9785353
Watheq <watheq.alshowaiter@gmail.com>
2019-11-09 22:33:52 +0300

    stativ inheritance behavior

--- static_modifier.php
+++ static_modifier.php
@@ -1,30 +1,50 @@
 <?php
 
-class Student
-{
-    public static $grades = ['freshman', 'somophor', "Junior", "Senior"];
-    private static $total_students =  0;
+class Student {
 
-    public static function motto()
-    {
-        return "To learn PHP OOP";
-    }
+  public static $grades = ['Freshman', 'Sophomore', 'Junior', 'Senior'];
+  private static $total_students = 0;
 
-    public static function count()
-    {
-        return self::$total_students;
-    }
+  public static function motto() {
+    return "To learn PHP OOP!";
+  }
 
-    public static function add_students()
-    {
-        self::$total_students++;
-    }
+  public static function count() {
+    return self::$total_students;
+  }
+
+  public static function add_student() {
+    self::$total_students++;
+  }
+
 }
 
-echo Student::$grades[0] . "<br>";
-echo Student::motto() . "<br>";
+echo Student::$grades[0] . "<br />";
+echo Student::motto() . "<br />";
 
-// echo Student::$total_students . "<br>";// ERROR 
-echo "Students count before: " .  Student::count() . "<br>";
-Student::add_students();
-echo "Students count after: " .  Student::count() . "<br>";
+// echo Student::$total_students . "<br />"; // Error
+echo Student::count() . "<br />";
+Student::add_student();
+echo Student::count() . "<br />";
+
+
+// Static properties and methods are inherited
+class PartTimeStudent extends Student {
+}
+
+echo PartTimeStudent::$grades[0] . "<br />";
+echo PartTimeStudent::motto() . "<br />";
+
+// Changes are shared too!
+PartTimeStudent::$grades[] = 'Alumni';
+echo implode(', ', Student::$grades) . "<br />";
+
+Student::add_student();
+Student::add_student();
+Student::add_student();
+PartTimeStudent::add_student();
+
+echo Student::count() . "<br />";
+echo PartTimeStudent::count() . "<br />";
+
+?>
