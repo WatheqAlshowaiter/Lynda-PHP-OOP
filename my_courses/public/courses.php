1c3516e5c66325706d19a37fd27d0ff57fcbf9bd
Watheq <watheq.alshowaiter@gmail.com>
2019-11-30 22:04:57 +0300

    some minor changes in css and about page and remove unncessary classes in courses.php

--- my_courses/public/courses.php
+++ my_courses/public/courses.php
@@ -16,53 +16,58 @@
             </div>
 
         </div>
-
-        <table class="rtl u-cf  ">
-            <thead>
-                <tr>
-                    <th>اسم الكورس</th>
-                    <th>المؤسسة</th>
-                    <th>مقدم المحتوى</th>
-                    <th>المستوى</th>
-                    <th>المادة</th>
-                    <th>اللغة</th>
-                    <th>الطول بالساعات</th>
-                    <th>مكتملة؟</th>
-                    <th>التقييم الشخصي</th>
-                    <th>تاريخ الإكمال</th>
-                    <th>الرابط</th>
-                    <th>ملاحظات</th>
-                </tr>
-            </thead>
-
-            <?php
-            $parse = new ParseCSV(PRIVATE_PATH . '/my_courses.csv');
-            $courses_array = $parse->parse();
-            ?>
-
-            <?php foreach ($courses_array as $args) : ?>
-
-                <?php $course = new Course($args); ?>
-
-                <tbody>
+        <div class="row">
+            <div class="twelve columns">
+            <table class="rtl courses-table">
+                <thead>
                     <tr>
-                        <td><?= h($course->name); ?> </td>
-                        <td><?= h($course->organization); ?> </td>
-                        <td><?= h($course->teacher); ?> </td>
-                        <td><?= h($course->level); ?> </td>
-                        <td><?= h($course->subject); ?> </td>
-                        <td><?= h($course->language); ?> </td>
-                        <td><?= h($course->long_in_hours); ?> </td>
-                        <td><?= h($course->is_complete); ?> </td>
-                        <td><?= h($course->rating); ?> </td>
-                        <td><?= h($course->date_complete); ?> </td>
-                        <td> <a  target="_blank" href=" <?= h($course->link); ?>">الرابط</a> </td>
-                        <td><?= h($course->notes); ?> </td>
+                        <th>اسم الكورس</th>
+                        <th>المؤسسة</th>
+                        <th>مقدم المحتوى</th>
+                        <th>المستوى</th>
+                        <th>المادة</th>
+                        <th>اللغة</th>
+                        <th>الطول بالساعات</th>
+                        <th>مكتملة؟</th>
+                        <th>التقييم الشخصي</th>
+                        <th>تاريخ الإكمال</th>
+                        <th>الرابط</th>
+                        <th>ملاحظات</th>
                     </tr>
-                </tbody>
+                </thead>
 
-            <?php endforeach; ?>
-        </table>
+                <?php
+                $parse = new ParseCSV(PRIVATE_PATH . '/my_courses.csv');
+                $courses_array = $parse->parse();
+                ?>
+
+                <?php foreach ($courses_array as $args) : ?>
+
+                    <?php $course = new Course($args); ?>
+
+                    <tbody>
+                        <tr>
+                            <td><?= h($course->name); ?> </td>
+                            <td><?= h($course->organization); ?> </td>
+                            <td><?= h($course->teacher); ?> </td>
+                            <td><?= h($course->level); ?> </td>
+                            <td><?= h($course->subject); ?> </td>
+                            <td><?= h($course->language); ?> </td>
+                            <td><?= h($course->long_in_hours); ?> </td>
+                            <td><?= h($course->is_complete); ?> </td>
+                            <td><?= h($course->rating); ?> </td>
+                            <td><?= h($course->date_complete); ?> </td>
+                            <td> <a target="_blank" href=" <?= h($course->link); ?>">الرابط</a> </td>
+                            <td><?= h($course->notes); ?> </td>
+                        </tr>
+                    </tbody>
+
+                <?php endforeach; ?>
+            </table>
+            </div>
+            
+        </div>
+
     </div>
 </div>
 
